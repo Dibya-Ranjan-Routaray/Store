e21c0c194460b353099331ee5fdb330c950fec3e
dibya <dibya2939@gmail.com>
2026-08-30 17:10:44 +0530

    Addition method add

--- src/main/java/com/nt/spring_git/controller/Test.java
+++ src/main/java/com/nt/spring_git/controller/Test.java
@@ -8,4 +8,7 @@ public class Test {
         SpringApplication.run(SpringGitApplication.class, args);
         System.out.println("Hello World");
     }
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
