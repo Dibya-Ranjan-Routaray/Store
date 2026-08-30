c10b0bc09cb7ffb492d0444217ed1d5db87e0856
dibya <dibya2939@gmail.com>
2026-08-30 17:03:07 +0530

    contact info found

--- src/main/java/com/nt/spring_git/controller/Test.java
+++ src/main/java/com/nt/spring_git/controller/Test.java
@@ -8,4 +8,7 @@ public class Test {
         SpringApplication.run(SpringGitApplication.class, args);
         System.out.println("Hello World");
     }
+    public String index(){
+        return "contact info found";
+    }
 }
